4e9e4c526f4bd31ea1951d3de6d8ef2a875fb49f
rednaxela700 <ohleg@windowslive.com>
2021-05-15 14:26:21 +0200

    exported game function and called in index file

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,8 @@
 // the scss file is imported directly to webpack
 // webpack compiles it and gives you a css output file
 import "../scss/main.scss";
+import "../assets/js/game.js"
+import game from "../assets/js/game.js";
+
 /* place your code below */
+game()
